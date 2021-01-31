2ffa66999c43e5abbd43f852102d317052028074
Rakibul Yeasin <rytotul@gmail.com>
2021-02-01 01:51:35 +0600

    Add large file :fire:

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-import os
-import json
-
-def main():
-    dirs = os.listdir()
-    target_dirs = []
-
-    for i in dirs:
-        if not os.path.isdir(i): dirs.remove(i)
-
-    for dir in dirs:
-        manifest = os.path.join(os.getcwd(), dir, 'manifest.json')
-        with open(manifest, 'r', encoding='utf-8') as f:
-            name = json.load(f)["name"]
-            target_dirs.append(name)
-
-    for i, j in zip(dirs, target_dirs):
-        os.rename(i, j)
-        print(f"[Rename] {i} -> {j}")
-
-if __name__ == "__main__":
-    main()
-
-"""
-modules
-function
-variable
-method
-list
-for loop
-conditional login
-file operation
-json
-dictionary
-
-"""
+version https://git-lfs.github.com/spec/v1
+oid sha256:42307df90c0c8c12fff61f6a3924273e35b886c034e410bb74410fc5c73c1abc
+size 618
